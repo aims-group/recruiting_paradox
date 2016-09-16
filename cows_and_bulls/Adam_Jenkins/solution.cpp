4c57fc5cdd0ddf29de1b49d24835ca57164efb87
adamjenkins1 <adamjenkins@comcast.net>
2016-09-16 11:53:00 -0700

    hiding user input in cows and bulls

--- cows_and_bulls/Adam_Jenkins/solution.cpp
+++ cows_and_bulls/Adam_Jenkins/solution.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <map>
 #include <algorithm>
+#include <termios.h>
+#include <unistd.h>
 using namespace std;
 
 void bullsAndCows(string &player1, string &player2, map<string, int> &bullsAndCowsMap)
@@ -30,14 +32,19 @@ int main()
   string player2 = "";
   char contd = 'Y';
   map<string, int> bullsAndCowsMap = {{"bulls", 0}, {"cows", 0}};
+  termios oldt;
+  tcgetattr(STDIN_FILENO, &oldt);
+  termios newt = oldt;
+  newt.c_lflag &= ~ECHO;
+  tcsetattr(STDIN_FILENO, TCSANOW, &newt);
   while(true)
   {
     cout << "Player 1: The word: ";
     cin >> player1;
-    cout << endl;
+    cout << endl << endl;
     cout << "Player 2: The guess: ";
     cin >> player2;
-    cout << endl;
+    cout << endl << endl;
     transform(player1.begin(), player1.end(), player1.begin(), ::toupper);
     transform(player2.begin(), player2.end(), player2.begin(), ::toupper);
     bullsAndCows(player1, player2, bullsAndCowsMap);
@@ -45,9 +52,11 @@ int main()
     cout << endl;
     cout << "Play again? [Y/n]" << endl;
     cin >> contd;
+    cout << endl;
     if(contd == 'n' || contd == 'N')
       break;
   }
+  tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
   cout << "Goodbye!" << endl;
 
   return 0;
